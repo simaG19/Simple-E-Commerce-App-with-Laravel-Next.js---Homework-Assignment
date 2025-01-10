29a538d8773822bed918ed990db383b6cd66e68e
simon <88380975+simaG19@users.noreply.github.com>
2025-01-10 14:33:33 +0300

    working on the api route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,15 +1,41 @@
 <?php
-
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+//Route for product
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/products', [ProductController::class, 'index']);
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::put('/products/{id}', [ProductController::class, 'update']);
-    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::post('/products', [ProductController::class, 'store']); // Admin only
+    Route::put('/products/{id}', [ProductController::class, 'update']); // Admin only
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']); // Admin only
+});
+
+
+//route for auth
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+
+use App\Http\Controllers\CartController;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/cart', [CartController::class, 'addToCart']);
+    Route::get('/cart', [CartController::class, 'viewCart']);
+});
+
+
+
+use App\Http\Controllers\OrderController;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/orders', [OrderController::class, 'placeOrder']);
 });
