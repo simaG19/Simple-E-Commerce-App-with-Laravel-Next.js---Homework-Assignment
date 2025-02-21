1eebd8bcdff71c27427403b291589dc210a55b3b
simon <88380975+simaG19@users.noreply.github.com>
2025-02-21 16:24:35 +0300

    update models

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -1,22 +1,11 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     //
-    use HasFactory;
-    protected $fillable = ['name', 'description', 'price', 'quantity', 'image_path'];
-
-    public function categories()
-    {
-        return $this->belongsToMany(Category::class);
-    }
-    public function orderItems()
-{
-    return $this->hasMany(OrderItem::class);
-}
 
 }
