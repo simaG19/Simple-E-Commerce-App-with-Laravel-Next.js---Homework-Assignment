e03c3e1336272a7e486e14bb61592b7c8272b269
simon <88380975+simaG19@users.noreply.github.com>
2025-02-21 16:23:42 +0300

    remove models

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -2,17 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
-
-    protected $fillable = ['name'];
+    //
 
     public function products()
-    {
-        return $this->belongsToMany(Product::class);
-    }
+{
+return $this->hasMany(Product::class);
+}
+
 }
