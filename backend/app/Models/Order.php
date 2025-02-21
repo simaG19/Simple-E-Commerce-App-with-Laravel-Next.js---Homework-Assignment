7af0ec7d3b0b7c1dd63af1e96de716e9c3e52773
simon <88380975+simaG19@users.noreply.github.com>
2025-02-21 16:24:09 +0300

    Update models

--- backend/app/Models/Order.php
+++ backend/app/Models/Order.php
@@ -2,23 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    use HasFactory;
-
-    protected $fillable = ['user_id', 'product_ids', 'total_price'];
-
+    //
     public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function items()
 {
-    return $this->hasMany(OrderItem::class);
+    return $this->belongsTo(User::class);
 }
 
 }
